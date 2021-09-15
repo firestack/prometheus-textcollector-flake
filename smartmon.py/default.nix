0acbbeafca938f071464ceabd80a37a7251e0163
Kayla Firestack <dev@kaylafire.me>
2021-09-15 12:20:53 -0400

    smartmon: add smartmontools to buildinputs

--- smartmon.py/default.nix
+++ smartmon.py/default.nix
@@ -1,11 +1,11 @@
-{ scripts, python3Packages }: let smartmon = python3Packages.buildPythonPackage {
+{ scripts, python3Packages, smartmontools }: let smartmon = python3Packages.buildPythonPackage {
   pname = "smartmon.py";
   version = "1";
   src = (toString scripts);
 
   Python = true;
   format = "other";
-
+  buildInputs = [ smartmontools ];
   installPhase = ''
   mkdir -p $out/bin
   cp $src/smartmon.py $out/bin/
