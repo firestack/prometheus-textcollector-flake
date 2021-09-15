61f88e542fa661be56fff11655cf16471b0d54a9
Kayla Firestack <dev@kaylafire.me>
2021-09-15 11:46:30 -0400

    smartmon# module: add more options, fix execstart

--- smartmon.py/module.nix
+++ smartmon.py/module.nix
@@ -3,13 +3,20 @@
 	options.services.smartmonpy = {
 		enable = lib.mkEnableOption "Enable Smartmon.py";
 
+		package = lib.mkOption {
+			description = "which program path to point to";
+			default = pkgs.smartmon-py;
+			type = lib.types.package;
+		};
 		extraOptions = lib.mkOption {
 			description = "Extra arguments passed to smartmonpy";
-			type = lib.types.str;
+			default = "";
+			type = with lib.types; listOf str;
 		};
-		
+
 		outFile = lib.mkOption {
 			description = "Which filepath the metrics output should be written";
+			default = "/var/lib/smartd-exporter/smartd.prom";
 			type = lib.types.path;
 		};
 
@@ -19,6 +26,6 @@
 	systemd.services.smartmonpy = {
 		description = "S.M.A.R.T.(smartmon py) prometheus collector";
 		wantedBy = [ "multi-user.target" ];
-		serviceConfig.ExecStart = "${cfg.program} ${lib.concatStringsSep " " cfg.extraOptions} > ${cfg.outFile}";
+		serviceConfig.ExecStart = "${cfg.package.meta.mainProgram} ${lib.concatStringsSep " " cfg.extraOptions} > ${cfg.outFile}";
 	};};
 }
