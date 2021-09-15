58135994699253786840372df7a7f70ac8d45660
Kayla Firestack <dev@kaylafire.me>
2021-09-15 11:15:38 -0400

    smartmon# module: add nixos module for testing

--- smartmon.py/module.nix
+++ smartmon.py/module.nix
@@ -1,3 +1,24 @@
-{ ... }: {
-    
+{ config, lib, pkgs, ... }: let cfg = config.services.smartmonpy; in {
+
+	options.services.smartmonpy = {
+		enable = lib.mkEnableOption "Enable Smartmon.py";
+
+		extraOptions = lib.mkOption {
+			description = "Extra arguments passed to smartmonpy";
+			type = lib.types.str;
+		};
+		
+		outFile = lib.mkOption {
+			description = "Which filepath the metrics output should be written";
+			type = lib.types.path;
+		};
+
+	};
+
+	config = lib.mkIf cfg.enable {
+	systemd.services.smartmonpy = {
+		description = "S.M.A.R.T.(smartmon py) prometheus collector";
+		wantedBy = [ "multi-user.target" ];
+		serviceConfig.ExecStart = "${cfg.program} ${lib.concatStringsSep " " cfg.extraOptions} > ${cfg.outFile}";
+	};};
 }
